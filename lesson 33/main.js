a6428e5e9dc7a0952d379412e182c20a7c30c1dc
MihaelMafiozi <74066035+MihaelMafiozi@users.noreply.github.com>
2021-02-26 19:01:19 +0200

    js

---
+++ lesson 33/main.js
@@ -0,0 +1,57 @@
+// console.log("" + 1 + 0)
+// console.log(true + false)
+// console.log(6 / "3")
+// console.log(4 + 5 + "px")
+// console.log("px" + 4 + 5)
+// console.log("-9" + 5)
+// console.log("-9" - 5)
+// console.log(5 > 4)
+// console.log("Яблоко" > "Яблочный")
+// console.log("2" > "12")
+// console.log("Vlad" == "Vladik")
+// let a = prompt("1 namber")
+//     b = prompt("2 namber")
+//     alert(a + b)
+// let q = Number(prompt("a:"))
+// if(q == 228){
+//     console.log("da")
+//     alert("ti chcolota")
+// }
+// else{
+//     alert("ti norm")
+//     console.log("net")
+// }
+// let a = Number(prompt("возраст"))
+// if(a >= 80){
+//     alert("НИЧЕСЕ ТЫ СТАРЫЙ")
+// }
+// if(a >= 18){
+//     alert("ПОРА РАБОТАТЬ")}
+// else(
+//     alert("ПШЕЛ ВОН")
+// )
+// console.log(a)
+// let a = (prompt("Возраст"))
+// if(a < 6){
+//     alert("ПШЕЛ ВОН")
+// }
+// else if(a > 16){
+//     alert("ПОРА РАБОТАТЬ")
+// }
+// else if("text")(
+//     alert("КТО ТЫ?")
+// )
+let a = Number(prompt("bal"))
+if(a <110){
+    alert("lox")
+}
+else if( a < 140){
+    alert("тоже лох")
+}
+else if(a <180 ){
+    alert("опять лох")
+}
+else{
+    alert("О ПОВЕЗЛО ПОВЕЗЛО")
+}
+console.log(a)
---
+++ lesson 35/main.js
@@ -0,0 +1,34 @@
+// let Bool = 1 || 0
+// alert(Bool)
+// let a = 1 || 1
+//     b = 0 || 1
+//     c = 0 || 0
+// alert(a)
+// alert(b)
+// alert(c)
+// let time = prompt("time")
+// if(time < 10 || time > 18){
+//     console.log("magazine closed")
+//     alert("magazine closed")  
+// }
+// else{
+//     alert("magazine work")
+//     console.log("magazine work")
+// }
+// let time = prompt("time")
+// if(time < 10 || time > 18){
+//     console.log("magazine closed")
+//     alert("magazine closed")  
+// }
+// else{
+//     alert("magazine work")
+//     console.log("magazine work")
+// }
+let Bool = 1 && 0
+    a = 1 && 1
+    b = 0 && 1
+    c = 0 && 0
+alert(Bool)
+alert(a)
+alert(b)
+alert(c)
