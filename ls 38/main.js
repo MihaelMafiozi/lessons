b175d66671477991a3b386f7d5413d5d11c855a8
MihaelMafiozi <74066035+MihaelMafiozi@users.noreply.github.com>
2021-03-22 18:50:16 +0200

    Add files via upload

---
+++ ls 38/main.js
@@ -0,0 +1,67 @@
+// let a = Number(prompt("число"))
+// if(a > 0){
+//     alert("+")
+// }
+// else{
+//     alert("-")
+// }
+
+// let a = prompt("a:")
+// if(a > 0){
+//     a++
+//     console.log(a)
+// }
+// else{
+//     a -=2
+//     console.log(a)
+// }
+
+// let a = prompt("a:")
+// if(a>0){
+//     a++
+//     console.log(a)
+// }
+// else if(a ==0){
+//     a=10
+//     console.log(a)
+// }
+// else{
+//     a -=2
+//     console.log(a)
+// }
+
+// let a = prompt("a:")
+//     b = prompt("b:")
+//     c = prompt("c:")
+// if(a >0 && b >0 && c >0){
+//     alert(3)
+// }
+// else if(a >0 && b >0 && c < 0){
+//     alert(2)
+// }
+// else if(a > 0 && b < 0 && c < 0){
+//     alert(1)
+// }
+// else if(a < 0 && b > 0 && c > 0){
+//     alert(2)
+// }
+// else if(a < 0 && b < 0 && c > 0){
+//     alert(1)
+// }
+// else if(a > 0 && b < 0 && c > 0){
+//     alert(2)
+// }
+// else if(a < 0 && b > 0 && c > 0){
+//     alert(2)
+// }
+// else if(a < 0 && b > 0 && c < 0){
+//     alert(1)
+// }
+// else if(a < 0 && b < 0 && c > 0){
+//     alert(1)
+// }
+
+let a = 1000
+while(a>0){
+    console.log(a)
+}
